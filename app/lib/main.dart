f8da5966e9ba65c7d56800aa625be62c340f5b7e
Vansh-77 <vanshbhardwaj7306@gmail.com>
2024-03-24 19:57:21 +0530

    init

---
+++ app/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:app/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          useMaterial3: true,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            hintStyle: const TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+            ),
+            labelStyle: const TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+            fillColor: const Color.fromARGB(255, 0, 40, 73),
+            filled: true,
+          )),
+      routes: {
+        "/": (context) => const Homepage(),
+      },
+    );
+  }
+}
