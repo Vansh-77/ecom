84f7faf7edfe8c9377a292723c85b80ccf0672f8
Vansh-77 <vanshbhardwaj733@gmail.com>
2024-04-19 17:31:12 +0530

    s

--- app/lib/detail.dart
+++ app/lib/detail.dart
@@ -1,6 +1,5 @@
 import 'package:app/model/products.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class Detailpage extends StatefulWidget {
   final prod item;
